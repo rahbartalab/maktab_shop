98661a6242719c981b91c98bcbfa493922e2616f
hossein <rahbartalab2002@gmail.com>
2024-01-25 16:52:32 +0330

    base structure for query builder prepared

--- index.php
+++ index.php
@@ -1,9 +1,22 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
+
 use Database\MysqlConnection;
+use Database\MysqlDatabase;
+
+$connection = MysqlConnection::getInstance();
+$connection->setPDO(new PDO('mysql:host=localhost;dbname=learnSQL', 'root'));
+$mysqlDatabase = new MysqlDatabase($connection);
 
 
-$connectionOne = MysqlConnection::getInstance();
-$connectionTwo = MysqlConnection::getInstance();
+//$bands  = $mysqlDatabase->table('bands')->select()->execute()->fetchAll();
+//$albums = $mysqlDatabase->table('albums')->select()->execute()->fetchAll();
 
-dd($connectionOne ,  $connectionTwo);
+$mysqlDatabase
+    ->table('songs')
+    ->delete()
+    ->where('id', '25', '=')
+    ->execute();
+
+
+
--- src/Database/MysqlConnection.php
+++ src/Database/MysqlConnection.php
@@ -2,9 +2,12 @@
 
 namespace Database;
 
+use PDO;
+
 class MysqlConnection implements DatabaseConnection
 {
     private static ?MysqlConnection $connection = null;
+    private PDO $pdo;
 
     private function __construct()
     {
@@ -14,5 +17,15 @@ class MysqlConnection implements DatabaseConnection
     {
         self::$connection = is_null(self::$connection) ? new MysqlConnection() : self::$connection;
         return self::$connection;
+    }
+
+    public function setPDO(PDO $pdo): void
+    {
+        $this->pdo = $pdo;
+    }
+
+    public function getPDO(): PDO
+    {
+        return $this->pdo;
     }
 }
