bffe31b6df52eebab127c40279b2b4859b9aaeee
hossein <rahbartalab2002@gmail.com>
2024-01-27 22:45:09 +0330

    [feat] mysql structure prepared

---
+++ src/Database/MysqlDatabase.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Database;
+
+use PDOStatement;
+use PDO;
+
+class MysqlDatabase implements DatabaseInterface
+{
+    private MysqlConnection $connection;
+    private string $table;
+    private string $query = '';
+    private PDOStatement $statement;
+
+    public function __construct(MysqlConnection $connection)
+    {
+        $this->connection = $connection;
+    }
+
+    public function getConnection(): MysqlConnection
+    {
+        return $this->connection;
+    }
+
+    public function table(string $table): self
+    {
+        $this->query = '';
+        $this->table = $table;
+        return $this;
+    }
+
+    public function insert(array $fields)
+    {
+        // TODO: Implement insert() method.
+    }
+
+    public function update(array $fields)
+    {
+        // TODO: Implement update() method.
+    }
+
+    public function select(array $columns = ['*'])
+    {
+        // ['*']
+        // ['id' , 'name'] -> id , name ...
+        // select * from table
+        $pattern = 'select %s from %s';
+        $this->query .= sprintf($pattern, implode(',', $columns), $this->table);
+        return $this;
+    }
+
+    public function delete(): self
+    {
+        $pattern = 'delete from %s';
+        $this->query .= sprintf($pattern, $this->table);
+        return $this;
+    }
+
+    public function execute(): self
+    {
+        $this->statement = $this->connection->getPDO()->prepare($this->query);
+        $this->statement->execute();
+        return $this;
+    }
+
+    public function fetchAll(): array
+    {
+        return $this->statement->fetchAll(PDO::FETCH_OBJ);
+    }
+
+    public function where(string $column, string $value, string $operation): self
+    {
+        // where $column $operation $value -> id(column) =(operation) 2(value)
+        $pattern = " where $column $operation $value";
+        $this->query .= $pattern;
+        return $this;
+    }
+}
